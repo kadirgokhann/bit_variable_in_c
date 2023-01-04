bbeff1193b45896b76bab380f5d062257f5cd602
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2023-01-04 19:35:22 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #define REG(X) (*(uint8_t *)&X)
-void decimalToBinary(int num);
+void dec2bin(int num);
 
 typedef struct {
     uint8_t dier: 1;
@@ -12,16 +12,16 @@ typedef struct {
 
 int main() {
     MyStruct my_struct = {0b1, 0b000, 0b1001};
-    decimalToBinary(REG(my_struct)); // 1001 000 1 => ldr = 0b1001, egr = 0b000, dier = 0b1 
+    dec2bin(REG(my_struct)); // 1001 000 1 => ldr = 0b1001, egr = 0b000, dier = 0b1 
     my_struct.egr = 0b111; 
     printf("\n");
-    decimalToBinary(REG(my_struct)); // 1001 111 1 => ldr = 0b1001, egr = 0b111, dier = 0b1
+    dec2bin(REG(my_struct)); // 1001 111 1 => ldr = 0b1001, egr = 0b111, dier = 0b1
     return 0;
 }
 
 
 
-void decimalToBinary(int num) {   
+void dec2bin(int num) {   
     if (num == 0) {
         printf("0");
         return;
