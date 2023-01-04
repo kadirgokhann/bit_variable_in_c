80d9be469fa71de9202e2b7d5950dd0eed9476aa
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2023-01-04 19:33:54 +0300

    Update main.c

--- main.c
+++ main.c
@@ -21,28 +21,18 @@ int main() {
 
 
 
-
-
-
-
-
-
 void decimalToBinary(int num) {   
     if (num == 0) {
         printf("0");
         return;
     }
-   
-   // Stores binary representation of number.
-   int binaryNum[32]; // Assuming 32 bit integer.
+   int b_num[32];
    int i=0;
    
    for ( ;num > 0; ){
-      binaryNum[i++] = num % 2;
+      b_num[i++] = num % 2;
       num /= 2;
    }
-   
-   // Printing array in reverse order.
    for (int j = i-1; j >= 0; j--)
-      printf("%d", binaryNum[j]);
+      printf("%d", b_num[j]);
 }
